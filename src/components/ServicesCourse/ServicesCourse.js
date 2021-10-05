c3e9ac384fb4c0df2f2f87aebc9f8ba4183ce48a
1Emonislam <emonislamweb@gmail.com>
2021-10-05 13:56:38 +0600

    spiner added

--- src/components/ServicesCourse/ServicesCourse.js
+++ src/components/ServicesCourse/ServicesCourse.js
@@ -1,6 +1,6 @@
 
 import { useEffect, useState } from 'react';
-import { Row } from 'react-bootstrap';
+import { Container, Row, Spinner } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import useCourses from '../../hooks/useCourses';
 import SingleCourse from '../SingleCourse/SingleCourse';
@@ -37,7 +37,7 @@ const ServicesCourse = () => {
                                     <li className="nav-item">
                                         <Link id="courses-list-tab" data-toggle="tab" to="#courses-list" role="tab" aria-controls="courses-list" aria-selected="false"><i className="fa fa-th-list" /></Link>
                                     </li>
-                                    <li className="nav-item">Showning {courses?.length} Results</li>
+                                    <li className="nav-item">Showning: {courses?.length} Results Showing:{searchDisplay?.length}</li>
                                 </ul> {/* nav */}
                                 <div className="courses-search float-right">
                                     <form action="#">
@@ -50,11 +50,14 @@ const ServicesCourse = () => {
                     </div> {/* row */}
                     <div className="tab-content" id="myTabContent">
                         <div className="tab-pane fade show active" id="courses-grid" role="tabpanel" aria-labelledby="courses-grid-tab">
-                            <Row xs={1} md={2} lg={3} xxl={4} className="g-4">
-                                {
-                                    searchDisplay.map(course => <SingleCourse key={course.id} course={course}></SingleCourse>)
-                                }
-                            </Row>
+                            {
+                                courses?.length === 0 ? <Container className="text-center my-5 spiner-show"> <Spinner animation="border" /></Container> :
+                                    <Row xs={1} md={2} lg={3} xxl={4} className="g-4">
+                                        {
+                                            searchDisplay.map(course => <SingleCourse key={course.id} course={course}></SingleCourse>)
+                                        }
+                                    </Row>
+                            }
                         </div>
                     </div>
                 </div>
